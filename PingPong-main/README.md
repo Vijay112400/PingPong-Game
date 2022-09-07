85454b0d316e3abace835430bd9c92db46ef395a
MUDAVATH VIJAY <88126518+Vijay112400@users.noreply.github.com>
2022-09-08 01:24:53 +0530

    First Commit

---
+++ PingPong-main/README.md
@@ -0,0 +1,39 @@
+# PingPong
+
+_Thanks to [**@AidanSun05**](https://github.com/AidanSun05) for resolving issues with Windows terminal. Now, This project is truly cross platform!_
+
+PingPong game built in C++. This game demonstrates several OOP concepts. Some of them are below.
+
+- Inheritance
+- Operator Overloading
+- Paramaterized Constructors
+- Code Splitting into multiple files (header files)
+- enums
+
+Ncurses library is used for console operations.
+
+### Installation & Change Log
+
+Refer [CHANGE_LOG](CHANGE_LOG.md). It also contains some additional details.
+
+## Play
+
+- Press `w` and `s` for left player movement. Up and Down respectively.
+- Press `i` and `k` for right player movement. Up and Down respectively.
+
+## Download
+
+Choose your OS and Download the game [here](https://github.com/CITIZENDOT/PingPong/releases/tag/1.0.0).
+
+## Demo
+
+https://user-images.githubusercontent.com/52322531/127457485-e0e2adb1-a2ed-4ae6-a5ae-6a0b27f6f71e.mp4
+
+### Inspiration
+
+This project is inspired by [this](https://youtube.com/playlist?list=PLrjEQvEart7faAurUFBS0zHyxktPUuyEI) playlist. Also, I improved somethings over that.
+
+- Game doesn't glitch. It super smooth now.
+- Emojis & Better looking UI.
+- Proper Classes which inherit from each other so, less repeated code.
+- Last but not least, CROSS PLATFORM!
---
+++ PingPong-main/src/GameManager.cpp
@@ -0,0 +1,177 @@
+#include "GameManager.h"
+#include <stdlib.h>
+#include <time.h>
+
+bool GameManager::in(int low, int value, int high) {
+    return (low <= value) && (value <= high);
+}
+
+
+void GameManager::printEmoji(const std::wstring& str) {
+#ifdef _WIN32
+    addrawch(str[0]);
+#else
+    char dst[11];
+    wcstombs(dst, str.data(), sizeof dst);
+    addstr(dst);
+#endif
+}
+
+GameManager::GameManager(int w, int h) {
+    width = w;
+    height = h;
+    srand(time(0));
+    quit = false;
+    scoreA = scoreB = 0;
+    ball = Ball(w / 2, h / 2);
+    playerA = Paddle(1, h / 2 + 1);
+    playerB = Paddle(w - 2, h / 2 + 1);
+    printw("Is this character appearing as Football emoji? [Y/n]:  ");
+    printEmoji(NEW_BALL_SYMBOL);
+    int input = getch();
+    if (input == 'Y' || input == 'y') {
+        BALL_SYMBOL = NEW_BALL_SYMBOL;
+        PLAYER_A_SYMBOL = NEW_PLAYER_A_SYMBOL;
+        PLAYER_B_SYMBOL = NEW_PLAYER_B_SYMBOL;
+        FENCE = NEW_FENCE;
+    } else {
+        printw("\nIs this character appearing as Block character? [Y/n]:  ");
+        printEmoji(OLD_PLAYER_A_SYMBOL);
+        input = getch();
+        if (input == 'Y' || input == 'y') {
+            BALL_SYMBOL = OLD_BALL_SYMBOL;
+            PLAYER_A_SYMBOL = OLD_PLAYER_A_SYMBOL;
+            PLAYER_B_SYMBOL = OLD_PLAYER_B_SYMBOL;
+            FENCE = OLD_FENCE;
+        } else {
+            BALL_SYMBOL = FALLBACK_BALL_SYMBOL;
+            PLAYER_A_SYMBOL = FALLBACK_PLAYER_A_SYMBOL;
+            PLAYER_B_SYMBOL = FALLBACK_PLAYER_B_SYMBOL;
+            FENCE = FALLBACK_FENCE;
+        }
+    }
+}
+
+void GameManager::scoreUp(Paddle &player) {
+    if (player == playerA)
+        scoreA++;
+    else if (player == playerB)
+        scoreB++;
+    ball.reset();
+    playerA.reset();
+    playerB.reset();
+}
+
+void GameManager::draw() {
+    clear();
+    for (int i = 0; i < width + 2; i++)
+        printEmoji(FENCE);
+    printw("\n");
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int ballX = ball.getX(), ballY = ball.getY();
+            int playerAX = playerA.getX(), playerAY = playerA.getY();
+            int playerBX = playerB.getX(), playerBY = playerB.getY();
+
+            if (j == 0)
+                printEmoji(FENCE);
+
+            if (ballX == j && ballY == i) {
+                printEmoji(BALL_SYMBOL);
+                if (BALL_SYMBOL == NEW_BALL_SYMBOL)
+                    j += 1;
+            } else if (playerAX == j && in(playerAY, i, playerAY + PLAYER_HEIGHT - 1)) {
+                printEmoji(PLAYER_A_SYMBOL);
+                if (PLAYER_A_SYMBOL == NEW_PLAYER_A_SYMBOL)
+                    j += 1;
+            } else if (playerBX == j && in(playerBY, i, playerBY + PLAYER_HEIGHT - 1)) {
+                printEmoji(PLAYER_B_SYMBOL);
+                if (PLAYER_B_SYMBOL == NEW_PLAYER_B_SYMBOL)
+                    j += 1;
+            } else
+                printEmoji(EMPTY);
+
+            if (j == width - 1)
+                printEmoji(FENCE);
+        }
+        printw("\n");
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        printEmoji(FENCE);
+    printw("\n");
+    printw("ScoreA: %d\n", scoreA);
+    printw("ScoreB: %d\n", scoreB);
+    refresh();
+}
+
+void GameManager::input() {
+    ball.move();
+    int playerAY = playerA.getY();
+    int playerBY = playerB.getY();
+
+    timeout(75);
+    int ch = getch();
+    if (ch == UP_A && (playerAY - 1) > 0)
+        playerA.moveUp();
+    else if (ch == UP_B && (playerBY - 1) > 0)
+        playerB.moveUp();
+    else if (ch == DOWN_A && (playerAY + 1) < height)
+        playerA.moveDown();
+    else if (ch == DOWN_B && (playerBY + 1) < height)
+        playerB.moveDown();
+
+    if (ball.getDirection() == STOP)
+        ball.randomDirection();
+    if (ch == QUIT)
+        quit = true;
+}
+
+void GameManager::logic() {
+    int ballX = ball.getX(), ballY = ball.getY();
+    int playerAX = playerA.getX(), playerAY = playerA.getY();
+    int playerBX = playerB.getX(), playerBY = playerB.getY();
+
+    // Left Paddle
+    for (int i = 0; i < PLAYER_HEIGHT; i++)
+        if (ballX == (playerAX + 1) && ballY == (playerAY + i))
+            ball.changeDirection((Direction)((rand() % 3) + 4));
+
+    // Right Paddle
+    for (int i = 0; i < PLAYER_HEIGHT; i++)
+        if (ballX == (playerBX - 1) && ballY == (playerBY + i))
+            ball.changeDirection((Direction)((rand() % 3) + 1));
+
+    // Bottom Wall
+    if (ballY == (height - 1))
+        ball.changeDirection(ball.getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+
+    // Top Wall
+    if (ballY == 0)
+        ball.changeDirection(ball.getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+
+    // Right wall
+    if (ballX == (width - 1)) {
+        if (in(playerBY, ballY, playerBY + PLAYER_HEIGHT - 1))
+            scoreUp(playerB);
+        else
+            scoreUp(playerA);
+    }
+
+    // Left wall
+    if (ballX == 0) {
+        if (in(playerAY, ballY, playerAY + PLAYER_HEIGHT - 1))
+            scoreUp(playerA);
+        else
+            scoreUp(playerB);
+    }
+}
+
+void GameManager::run() {
+    while (!quit) {
+        draw();
+        input();
+        logic();
+    }
+}
